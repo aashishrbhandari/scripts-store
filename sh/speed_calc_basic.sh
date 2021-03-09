c7310f61ac63b170b001d6b5bcabd8a4f6887055
Ashish R Bhandari <67356322+Asharry97@users.noreply.github.com>
2021-03-09 08:20:05 +0530

    A Simple Realtime Speed Calc
    A Simple Speed Calc Shows Realtime Speed of ALL NIC

---
+++ sh/speed_calc_basic.sh
@@ -0,0 +1,56 @@
+#### Step1 : Get All Nic Card
+#ls -l /sys/class/net/ | grep ^l | tr -s " " | cut -d" " -f9 | grep -v "lo"
+#
+#### Step [Optional]
+## If NIC card is UP or Down: cat /sys/class/net/eth0/carrier # 1 is UP , 0 is DOWN
+#
+#### Step2: Get Each NIC TX(Transmitted Traffic/Send from this NIC) & RX(Received On this NIC)
+#cat /sys/class/net/eth0/statistics/rx_bytes
+#cat /sys/class/net/eth0/statistics/tx_bytes
+
+function bytesToHR() {
+  local SIZE=$1
+  local UNITS="B/s KB/s MB/s GB/s TB/s PB/s"
+  for F in $UNITS; do
+    local UNIT=$F
+    test ${SIZE%.*} -lt 1024 && break;
+    SIZE=$(echo "$SIZE / 1024" | bc -l)
+  done
+
+  if [ "$UNIT" == "B/s" ]; then
+    printf "%4.0f%s\n" $SIZE $UNIT
+  else
+    printf "%7.02f%s\n" $SIZE $UNIT
+  fi
+}
+
+
+# New Dict Kindof Array
+declare -A NIC_DICT
+
+ALL_NIC=$(ls -l /sys/class/net/ | grep ^l | tr -s " " | cut -d" " -f9 | grep -v "lo")
+
+for ONE_NIC in ${ALL_NIC};
+do
+    START_RX_BYTES=$(cat /sys/class/net/${ONE_NIC}/statistics/rx_bytes);
+    START_TX_BYTES=$(cat /sys/class/net/${ONE_NIC}/statistics/tx_bytes);
+    NIC_DICT["${ONE_NIC}_RX"]=${START_RX_BYTES}
+    NIC_DICT["${ONE_NIC}_TX"]=${START_TX_BYTES}
+done
+
+sleep 1s;
+
+for ONE_NIC in ${ALL_NIC};
+do
+    NOW_RX_BYTES=$(cat /sys/class/net/${ONE_NIC}/statistics/rx_bytes);
+    NOW_TX_BYTES=$(cat /sys/class/net/${ONE_NIC}/statistics/tx_bytes);
+    
+    SPEED_RX=$(echo $(( $NOW_RX_BYTES - ${NIC_DICT["${ONE_NIC}_RX"]} )) | bc);
+    SPEED_TX=$(echo $(( $NOW_TX_BYTES - ${NIC_DICT["${ONE_NIC}_TX"]} )) | bc);
+    SPEED_RX_CALC=$(bytesToHR ${SPEED_RX});
+    SPEED_TX_CALC=$(bytesToHR ${SPEED_TX});
+
+    TOTAL_SPEED=$(( ${SPEED_RX} + ${SPEED_TX} ));
+    TOTAL_SPEED_CALC=$(bytesToHR ${TOTAL_SPEED});
+    printf "%-30s| %-20s%-20s%-20s%-20s\n" "NIC:[${ONE_NIC}]" "RX: ${SPEED_RX_CALC}" "TX: ${SPEED_TX_CALC}" "TOTAL: ${TOTAL_SPEED_CALC}"
+done
