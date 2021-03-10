85dcda61a3899a7519f78383a612db423220f17c
Ashish R Bhandari <67356322+Asharry97@users.noreply.github.com>
2021-03-10 11:25:05 +0530

    Update speed_calc_basic.sh

--- sh/speed_calc_basic.sh
+++ sh/speed_calc_basic.sh
@@ -4,20 +4,37 @@
 #### Step [Optional]
 ## If NIC card is UP or Down: cat /sys/class/net/eth0/carrier # 1 is UP , 0 is DOWN
 #
-#### Step2: Get Each NIC TX(Transmitted Traffic/Send from this NIC) & RX(Received On this NIC)
+#### Get Each NIC TX(Transmitted Traffic/Send from this NIC) & RX(Received On this NIC)
 #cat /sys/class/net/eth0/statistics/rx_bytes
 #cat /sys/class/net/eth0/statistics/tx_bytes
 
-function bytesToHR() {
+function BYTES_TO_HR() {
   local SIZE=$1
-  local UNITS="B/s KB/s MB/s GB/s TB/s PB/s"
+  local UNITS="B KB MB GB TB PB"
   for F in $UNITS; do
     local UNIT=$F
     test ${SIZE%.*} -lt 1024 && break;
     SIZE=$(echo "$SIZE / 1024" | bc -l)
   done
 
-  if [ "$UNIT" == "B/s" ]; then
+  if [ "$UNIT" == "B" ]; then
+    printf "%4.0f%s\n" $SIZE $UNIT
+  else
+    printf "%7.02f%s\n" $SIZE $UNIT
+  fi
+}
+
+function BYTES_TO_HR() {
+  local SIZE=$1
+  SIZE=$(( ${SIZE}*8 ));
+  local UNITS="b Kb Mb Gb Tb Pb"
+  for F in $UNITS; do
+    local UNIT=$F
+    test ${SIZE%.*} -lt 1024 && break;
+    SIZE=$(echo "$SIZE / 1024" | bc -l)
+  done
+
+  if [ "$UNIT" == "b" ]; then
     printf "%4.0f%s\n" $SIZE $UNIT
   else
     printf "%7.02f%s\n" $SIZE $UNIT
@@ -44,13 +61,12 @@ for ONE_NIC in ${ALL_NIC};
 do
     NOW_RX_BYTES=$(cat /sys/class/net/${ONE_NIC}/statistics/rx_bytes);
     NOW_TX_BYTES=$(cat /sys/class/net/${ONE_NIC}/statistics/tx_bytes);
-    
     SPEED_RX=$(echo $(( $NOW_RX_BYTES - ${NIC_DICT["${ONE_NIC}_RX"]} )) | bc);
     SPEED_TX=$(echo $(( $NOW_TX_BYTES - ${NIC_DICT["${ONE_NIC}_TX"]} )) | bc);
-    SPEED_RX_CALC=$(bytesToHR ${SPEED_RX});
-    SPEED_TX_CALC=$(bytesToHR ${SPEED_TX});
+    SPEED_RX_CALC=$(BYTES_TO_HR ${SPEED_RX})
+    SPEED_TX_CALC=$(BYTES_TO_HR ${SPEED_TX})
 
     TOTAL_SPEED=$(( ${SPEED_RX} + ${SPEED_TX} ));
-    TOTAL_SPEED_CALC=$(bytesToHR ${TOTAL_SPEED});
+    TOTAL_SPEED_CALC=$(BYTES_TO_HR ${TOTAL_SPEED});
     printf "%-30s| %-20s%-20s%-20s%-20s\n" "NIC:[${ONE_NIC}]" "RX: ${SPEED_RX_CALC}" "TX: ${SPEED_TX_CALC}" "TOTAL: ${TOTAL_SPEED_CALC}"
 done
